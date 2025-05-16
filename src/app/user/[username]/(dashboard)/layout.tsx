ef4d7ef71b484f3cbf286881d830355703d84c24
minhtq05 <minhtq2005@gmail.com>
2025-05-16 10:23:18 +0700

    add user level routes

--- src/app/user/[username]/(dashboard)/layout.tsx
+++ src/app/user/[username]/(dashboard)/layout.tsx
@@ -1,11 +1,20 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { DashboardSidebar } from "@/components/dashboard-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
+  params,
   children,
 }: {
+  params: { user: string };
   children: React.ReactNode;
 }) {
+  const user = await currentUser();
+
+  if (user === null || user.username !== params.user) {
+    return <p>You do not have access to this page</p>;
+  }
+
   return (
     <SidebarProvider>
       <DashboardSidebar />
