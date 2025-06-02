1fde7c0f5ed39680da9e20b2f7e7fdc781a64560
minhtq05 <minhtq2005@gmail.com>
2025-06-02 21:07:13 +0700

    add turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // Example: adding an alias and custom file extension
+    resolveAlias: {
+      underscore: "lodash",
+    },
+    resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+  // output: "standalone",
 };
 
 export default nextConfig;
